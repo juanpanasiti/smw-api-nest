65bc5140fe1ac72e3296d319e9fa3b220b9fec9b
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-03-15 17:26:01 -0300

    create profile resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, ProfileModule],
   controllers: [],
   providers: [],
 })
