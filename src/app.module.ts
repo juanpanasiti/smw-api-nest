e098ad98246a24cd17fda4c0228a7dce02d9c572
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-03-16 15:16:28 -0300

    create expense resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ProfileModule } from './profile/profile.module';
 import { CreditCardsModule } from './credit-cards/credit-cards.module';
+import { ExpensesModule } from './expenses/expenses.module';
 
 @Module({
-  imports: [AuthModule, ProfileModule, CreditCardsModule],
+  imports: [AuthModule, ProfileModule, CreditCardsModule, ExpensesModule],
   controllers: [],
   providers: [],
 })
