a336dae64711f197851138798d368ec95f08afd0
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-03-16 15:21:10 -0300

    set properties to create an expense

--- src/expenses/dto/create-expense.dto.ts
+++ src/expenses/dto/create-expense.dto.ts
@@ -1,1 +1,10 @@
-export class CreateExpenseDto {}
+export class CreateExpenseDto {
+  title: string;
+  ccName: string;
+  acquiredAt: Date;
+  // creditCardId: mongoId
+  type: 'subscription' | 'purchase';
+  amount: number;
+  installments: number;
+  isActive: boolean; // true by default
+}
