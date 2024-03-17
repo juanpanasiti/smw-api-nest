17bbfb239790147a219288c879809de4c71e4839
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-03-17 19:57:34 -0300

    Implement validations on profile DTOs

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -1,5 +1,16 @@
+import { IsDateString, IsOptional, IsString } from 'class-validator';
+
 export class CreateProfileDto {
-  firstName: string;
-  lastName: string;
-  birthDate: string;
+  // @IsMongoId
+  // readonly userId: mongoId
+
+  @IsString()
+  readonly firstName: string;
+
+  @IsString()
+  readonly lastName: string;
+
+  @IsDateString({ strictSeparator: false, strict: true })
+  @IsOptional()
+  readonly birthDate?: string;
 }
