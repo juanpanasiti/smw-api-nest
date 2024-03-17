b3990745fb689a02829968d9d6455e3d6c42fd84
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-03-17 20:32:00 -0300

    set global prefix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api/v2')
+
   // Global Pipes
   app.useGlobalPipes(
     new ValidationPipe({
