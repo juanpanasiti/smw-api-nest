f408db0722c18b53d4898d5d43a562d003e481f3
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-03-17 19:47:33 -0300

    Implement validations on expense DTOs

--- src/expenses/dto/create-expense.dto.ts
+++ src/expenses/dto/create-expense.dto.ts
@@ -1,10 +1,29 @@
+import { IsBoolean, IsDecimal, IsInt, IsPositive, IsString, MaxDate } from "class-validator";
+
 export class CreateExpenseDto {
-  title: string;
-  ccName: string;
-  acquiredAt: Date;
-  // creditCardId: mongoId
-  type: 'subscription' | 'purchase';
-  amount: number;
-  installments: number;
-  isActive: boolean; // true by default
+  @IsString()
+  readonly title: string;
+
+  @IsString()
+  readonly ccName: string;
+  
+  @MaxDate(new Date(), {message: 'must be today or before'})
+  readonly acquiredAt: Date;
+
+  // @IsMongoId
+  // readonly creditCardId: mongoId
+
+  @IsString()
+  readonly type: 'subscription' | 'purchase';
+
+  @IsPositive()
+  @IsDecimal({decimal_digits: '0,2'})
+  readonly amount: number;
+
+  @IsPositive()
+  @IsInt()
+  readonly installments: number;
+
+  @IsBoolean()
+  readonly isActive: boolean; // true by default
 }
--- src/expenses/dto/update-expense.dto.ts
+++ src/expenses/dto/update-expense.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { OmitType } from '@nestjs/mapped-types';
 import { CreateExpenseDto } from './create-expense.dto';
 
-export class UpdateExpenseDto extends PartialType(CreateExpenseDto) {}
+export class UpdateExpenseDto extends OmitType(CreateExpenseDto, ['type']) {}
