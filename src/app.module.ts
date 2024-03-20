91d77894ae3f18d4d3f1863079e0cf88e6ab8c00
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-03-19 22:13:32 -0300

    implement MongoDB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AuthModule } from './auth/auth.module';
 import { ProfileModule } from './profile/profile.module';
@@ -7,7 +8,14 @@ import { CreditCardsModule } from './credit-cards/credit-cards.module';
 import { ExpensesModule } from './expenses/expenses.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, ProfileModule, CreditCardsModule, ExpensesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    ProfileModule,
+    CreditCardsModule,
+    ExpensesModule,
+    MongooseModule.forRoot(process.env.MONGODB)
+  ],
   controllers: [],
   providers: [],
 })
