8be2d186cf38f01f57245757ea94278fa6359337
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-03-19 22:28:46 -0300

    config user service with schema

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { User } from './entities/user.entity';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<User>,
+  ) {}
+
   register(registerUserDto: RegisterUserDto) {
-    return 'This action register a new user';
+    //
   }
 
   login(loginUserDto: LoginUserDto) {
