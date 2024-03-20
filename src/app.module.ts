e054a941da16a5f53f02c0cb72d18ec0d56aebe8
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-03-19 22:01:02 -0300

    add dependency for .env file and set port

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
 import { ProfileModule } from './profile/profile.module';
 import { CreditCardsModule } from './credit-cards/credit-cards.module';
 import { ExpensesModule } from './expenses/expenses.module';
 
 @Module({
-  imports: [AuthModule, ProfileModule, CreditCardsModule, ExpensesModule],
+  imports: [ConfigModule.forRoot(), AuthModule, ProfileModule, CreditCardsModule, ExpensesModule],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
