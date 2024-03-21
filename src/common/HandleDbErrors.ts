1f1a082d3a872b533a3e39245de2fcaf2d7ecc74
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-03-21 20:10:41 -0300

    improve errors handling

--- src/common/HandleDbErrors.ts
+++ src/common/HandleDbErrors.ts
@@ -1,10 +1,14 @@
 import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { MongooseError } from 'mongoose';
 
 export class HandleDbErrors {
-  public static  handle(error: any) {
-    if (error.code === 'some') throw new BadRequestException(error.detail);
+  private static DUPLICATE_KEY_CODE = 11000;
+  public static handle(error: any): never {
+    if (error.code === this.DUPLICATE_KEY_CODE) throw new BadRequestException(`Duplicated key ${JSON.stringify(error['keyValue'])}`);
 
-    console.log(error)
-    throw new InternalServerErrorException('Internal Server Error, contact sysadmin')
+    console.log(error);
+    if (error instanceof MongooseError) throw new InternalServerErrorException('Unhandled error with db operation.');
+
+    throw new InternalServerErrorException('Internal Server Error, contact sysadmin');
   }
 }
