f7e78ae36985a364335814749403c4e3b6b622bb
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-03-21 20:21:48 -0300

    add validation error handling

--- src/common/HandleDbErrors.ts
+++ src/common/HandleDbErrors.ts
@@ -3,8 +3,11 @@ import { MongooseError } from 'mongoose';
 
 export class HandleDbErrors {
   private static DUPLICATE_KEY_CODE = 11000;
+  private static VALIDATION_ERROR_CODE = 'ValidationError'
+
   public static handle(error: any): never {
     if (error.code === this.DUPLICATE_KEY_CODE) throw new BadRequestException(`Duplicated key ${JSON.stringify(error['keyValue'])}`);
+    if (error.name === this.VALIDATION_ERROR_CODE) throw new BadRequestException(error.message)
 
     console.log(error);
     if (error instanceof MongooseError) throw new InternalServerErrorException('Unhandled error with db operation.');
