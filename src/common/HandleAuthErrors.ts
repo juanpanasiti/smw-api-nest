053ea1db3eb4fb22f283686d83af847e161d6278
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-03-23 19:35:03 -0300

    implement invalid token error

--- src/common/HandleAuthErrors.ts
+++ src/common/HandleAuthErrors.ts
@@ -2,13 +2,19 @@ import { InternalServerErrorException, UnauthorizedException } from '@nestjs/com
 
 export class HandleAuthErrors {
   private static LOGIN_ERROR = 'LOGIN_ERROR';
+  private static INVALID_TOKEN = 'INVALID_TOKEN';
 
   public static handle(error: any): never {
     if (error === this.LOGIN_ERROR) throw new UnauthorizedException('Invalid credentials');
+    if (error === this.INVALID_TOKEN) throw new UnauthorizedException('Invalid Token')
     throw new InternalServerErrorException('Internal Server Error, contact sysadmin');
   }
 
   public static loginError() {
     this.handle(this.LOGIN_ERROR);
+  }
+
+  public static invalidToken() {
+    this.handle(this.INVALID_TOKEN);
   }
 }
