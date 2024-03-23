19b48b6b59fb1b8157445b8440949f7e372b1fab
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-03-23 18:53:50 -0300

    Implements Passport and JWT config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User, UserSchema } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
@@ -15,6 +17,14 @@ import { User, UserSchema } from './entities/user.entity';
         schema: UserSchema,
       },
     ]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    // Sync example
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
   ],
 })
 export class AuthModule {}
