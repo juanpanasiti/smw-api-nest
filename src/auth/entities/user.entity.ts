8539dceafebe24cbf0b0a279931cebfbd55ec7e5
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-03-27 00:44:34 -0300

    add roles to entity

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,6 +1,8 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
+import { ValidRoles } from '../interfaces';
+
 @Schema()
 export class User extends Document {
   @Prop({ unique: true, index: true, required: true })
@@ -9,8 +11,11 @@ export class User extends Document {
   @Prop({ unique: true, index: true, required: true })
   email: string;
 
-  @Prop({required: true, select: false})
+  @Prop({ required: true, select: false })
   password: string;
+
+  @Prop({ default: [ValidRoles.user] })
+  roles: ValidRoles[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
