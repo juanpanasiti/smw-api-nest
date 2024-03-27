e3ceb4a9236e1af62fcfb91b8818b2194319aec6
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-03-27 00:44:01 -0300

    add custom decorators and guards for auth

---
+++ src/auth/decorators/role-protected/role-protected.decorator.ts
@@ -0,0 +1,8 @@
+import { SetMetadata } from '@nestjs/common';
+import { ValidRoles } from 'src/auth/interfaces';
+
+export const META_ROLES = 'roles';
+
+export const RoleProtected = (...args: ValidRoles[]) => {
+  return SetMetadata(META_ROLES, args);
+};
