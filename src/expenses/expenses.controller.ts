be4c40985e55bca17958a0d0fa7fd9e6925e9e5b
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-03-26 23:50:18 -0300

    improve expenses dtos import exports

--- src/expenses/expenses.controller.ts
+++ src/expenses/expenses.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+
 import { ExpensesService } from './expenses.service';
-import { CreateExpenseDto } from './dto/create-expense.dto';
-import { UpdateExpenseDto } from './dto/update-expense.dto';
+import { CreateExpenseDto, UpdateExpenseDto } from './dto';
 
 @Controller('expenses')
 export class ExpensesController {
--- src/expenses/expenses.service.ts
+++ src/expenses/expenses.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { CreateExpenseDto } from './dto/create-expense.dto';
-import { UpdateExpenseDto } from './dto/update-expense.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { Expense } from './entities/expense.entity';
 import { Model } from 'mongoose';
+
+import { Expense } from './entities/expense.entity';
+import { CreateExpenseDto, UpdateExpenseDto } from './dto';
 
 @Injectable()
 export class ExpensesService {
