4a58b314f3f6d393fac0c6fdc6319da3e5e4f661
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-04-03 01:31:21 -0300

    add not found error

--- src/common/error-handlers/HandleHttpErrors.ts
+++ src/common/error-handlers/HandleHttpErrors.ts
@@ -1,8 +1,12 @@
 import { BadRequestException } from '@nestjs/common';
 
 export class HandleHttpErrors {
-  private static BAD_REQUEST_DEFAULT_MSG = 'No specified';
+  private static BAD_REQUEST_DEFAULT_MSG = 'Not specified';
+  private static NOT_FOUND_DEFAULT_MSG = 'Not specified';
   public static badRequest(message: string = this.BAD_REQUEST_DEFAULT_MSG): never {
     throw new BadRequestException(`Bad Request: ${message}`);
+  }
+  public static notFound(message: string = this.NOT_FOUND_DEFAULT_MSG): never {
+    throw new BadRequestException(`Not Found: ${message}`);
   }
 }
