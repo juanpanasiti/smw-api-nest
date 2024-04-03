9b75ca735e449ae09c657d7b0d05e14c37825d5c
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-04-03 00:46:15 -0300

    fix decimals to amount fields

--- src/common/constants/transform-doc.ts
+++ src/common/constants/transform-doc.ts
@@ -6,5 +6,6 @@ export const transformDoc = (doc: Doc, ret: Ret) => {
   ret.id = ret._id;
   delete ret._id;
   delete ret.isActive;
+  if(ret.amount && typeof ret.amount === 'number') ret.amount = +ret.amount.toFixed(2)
   return ret;
 };
